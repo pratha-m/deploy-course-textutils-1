8376dc103ea652abe0e49cc95a4987a730a1d169
pratham <prathamchhabra85@gmail.com>
2022-05-12 20:38:31 +0530

    uploading textutils 1

--- revision/import_export_modules/module1.mjs
+++
@@ -1,5 +0,0 @@
-import hello,{a,c,d} from "./module2.mjs"
-console.log(hello); 
-console.log(a);
-console.log(c);
-console.log(d);
--- revision/import_export_modules/module2.mjs
+++
@@ -1,9 +0,0 @@
-const a="pratham";
-const b="aakash";
-const c="harry";
-const d="chhabra";
-
-export default b; // b is export by default
-export {a};
-export {c};
-export {d};
