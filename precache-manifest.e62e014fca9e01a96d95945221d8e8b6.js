8f981b9ec87d32fb8a06490c9a017d8c6d2c6e1e
pratham <prathamchhabra85@gmail.com>
2022-05-12 21:01:32 +0530

    Updates

---
+++ precache-manifest.e62e014fca9e01a96d95945221d8e8b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14e4af0f5254e0323071",
+    "url": "/deploy-course-textutils-1/static/css/main.feae8c45.chunk.css"
+  },
+  {
+    "revision": "14e4af0f5254e0323071",
+    "url": "/deploy-course-textutils-1/static/js/main.14e4af0f.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/deploy-course-textutils-1/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "9dcc9841af8ba5a34bee",
+    "url": "/deploy-course-textutils-1/static/js/2.9dcc9841.chunk.js"
+  },
+  {
+    "revision": "f2070baab124db300925",
+    "url": "/deploy-course-textutils-1/static/js/runtime~main.f2070baa.js"
+  },
+  {
+    "revision": "0ff92fcce9e1112c757a48b8c83be798",
+    "url": "/deploy-course-textutils-1/index.html"
+  }
+];
